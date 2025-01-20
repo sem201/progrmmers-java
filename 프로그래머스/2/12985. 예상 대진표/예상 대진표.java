286693d2a3c99efe5b972957e9f49ee483bcb955
sjae_o <38523683+sem201@users.noreply.github.com>
2025-01-20 11:59:54 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,17 @@
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 0;
+        while(true){
+            a = a%2==0 ? a/2: a/2+1;
+            b = b%2 ==0 ? b/2: b/2+1;
+            answer ++;
+            if(a==b){
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
