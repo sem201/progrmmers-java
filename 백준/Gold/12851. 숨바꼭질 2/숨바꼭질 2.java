0228609660d5a87d9f78ccf7c4f9e7a384b48544
sjae_o <38523683+sem201@users.noreply.github.com>
2025-10-18 05:25:26 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 176 ms, Memory: 22948 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Array;
+import java.util.*;
+
+public class Main {
+    static int N,K;
+    static int minTime = Integer.MAX_VALUE;
+    static int[] time = new int [100001];
+    static int count =0;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        if(N >=K){
+            System.out.println((N-K) + "\n1");
+            return;
+        }
+        bfs();
+
+        System.out.println(minTime + "\n" + count);
+    }
+    public static void bfs(){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(N);
+        time[N] = 1;
+        while (!q.isEmpty()){
+            int now = q.poll();
+
+            if(minTime < time[now]) return;
+            for(int i=0; i<3; i++){
+                int next;
+
+                if(i==0){
+                    next = now-1;
+                } else if (i==1) {
+                    next = now+1;
+                }
+                else{
+                    next = now*2;
+                }
+
+                if(next <0 || next > 100000) continue;
+
+                if(next == K){
+                    minTime = time[now];
+                    count++;
+                }
+
+                if(time[next] == 0 || time[next] == time[now]+1){
+                    q.add(next);
+                    time[next] = time[now] + 1;
+                }
+            }
+        }
+    }
+    
+
+}
